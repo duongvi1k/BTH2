b4f8694d1d6641d71e4dda00473d8e9b2e8f7d94
duongvi1k <duongvi1k2@gmail.com>
2025-10-06 08:59:17 +0700

    Behavioral

---
+++ DPM225500_DuongTriVi_Pattern11_AbstractFactory/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225500_DuongTriVi_Pattern11_AbstractFactory
+{
+    internal class Client
+    {
+    }
+}
---
+++ DPM225500_DuongTriVi_Pattern12_Builder/Builder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225500_DuongTriVi_Pattern12_Builder
+{
+    internal class Builder
+    {
+    }
+}
